7fc13cd0376d32533d8dfa1dae491f6940601bf2
Kaja Mikolajczyk <kajaths@gmail.com>
2018-05-02 14:11:46 +0000

    Added and tested functions creating cookbook and finding them

--- run.py
+++ run.py
@@ -1,6 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request, url_for
 import pymongo
+from bson.objectid import ObjectId
 from flask_pymongo import PyMongo
 from secret import db_name, uri_str
 import datetime
@@ -20,14 +21,29 @@ def create_nice_date():
     new_date = "{0}-{1}-{2}".format(now.day,now.month,now.year)
     return new_date
 
+def create_cookbook(cookbook_name='', password='', username=''):
+    """
+    """
+    _id=mongo.db.cookbooks.insert_one({"cookbok_name": cookbook_name,
+        "password" : password,
+        "author_name": username,
+        "created_on": create_nice_date(),
+        "recipes_pinned": [],
+        "recipes_owned": []
+    })
+    
+    return _id
+
 def say_hello():
     return "hello"
     
-def homepage(query={}):
+def get_recipes(query={}):
     
     return mongo.db.recipes.find(query)
 
 
+def get_cookbooks(query={}):
+    return mongo.db.cookbooks.find_one(query)
 
 
 if __name__ == "__main__":
--- tests.py
+++ tests.py
@@ -2,3 +2,30 @@ import run
 import unittest
 
 
+#c = run.app.test_client()
+
+class TestCookbook(unittest.TestCase):
+    
+    
+    
+    def test_home_page(self):
+        
+        self.assertEqual("hello", run.say_hello())
+        
+    def test_recipes(self):
+        with run.app.app_context():
+            self.assertIsNotNone(run.get_recipes(query={}))
+          
+          
+    def  test_creating_cookbook(self):
+        username = "Sebastian"
+        with run.app.app_context():
+            username = "Sebastian"
+            created_id=run.create_cookbook(cookbook_name="Seb_cookbook", password="", username=username)
+            item=run.get_cookbooks(query={"author_name": "Sebastian"})
+    
+            self.assertEquals(created_id, item.["_id"])
+            
+
+if __name__ == '__main__':
+    unittest.main()
