00854c30b29c0603269f905a015545cdcb870162
Kaja Mikolajczyk <kajaths@gmail.com>
2018-09-25 18:47:58 +0000

    Fixed some other variable issues

--- app.py
+++ app.py
@@ -1,6 +1,5 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-from secret import secret_key
 
 
 UP_FOLDER = 'static/uploaded_images/'
@@ -8,7 +7,6 @@ def create_app(conf_obj='conf.TestingConfig'):
     
     application = Flask(__name__)
     application.config.from_object(conf_obj)
-    application.secret_key = secret_key
     
     return application
 # setting up configuration
