328bd67afb2e41daa3c289234cb483984ffa0675
Kaja Mikolajczyk <kajaths@gmail.com>
2018-09-25 18:34:49 +0000

    Fixed some other variable issues

--- app.py
+++ app.py
@@ -1,18 +1,20 @@
 from flask import Flask
 from flask_pymongo import PyMongo
-from basic import UP_FOLDER
 from secret import secret_key
 
 
-
+UP_FOLDER = 'static/uploaded_images/'
 def create_app(conf_obj='conf.TestingConfig'):
     
     application = Flask(__name__)
     application.config.from_object(conf_obj)
     application.secret_key = secret_key
+    
     return application
 # setting up configuration
 #('Config')
+
 app = create_app(conf_obj='conf.Config')
+app.config['UPLOAD_FOLDER'] = UP_FOLDER
 mongo = PyMongo(app)
 
--- views.py
+++ views.py
@@ -1,7 +1,6 @@
 from flask import Flask, render_template, redirect, request, url_for, flash, session
 import pymongo
 from bson.objectid import ObjectId
-from basic import UP_FOLDER
 from secret import secret_key
 from app import app, mongo
 from werkzeug.utils import secure_filename
