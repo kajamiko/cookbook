2bd51790265b29f5da62ee0b5385919a2d6148c0
Kaja Mikolajczyk <kajaths@gmail.com>
2018-09-26 17:55:44 +0000

    Heroku

--- app.py
+++ app.py
@@ -3,20 +3,25 @@ import os
 from flask_pymongo import PyMongo
 
 UP_FOLDER = 'static/uploaded_images/'
-def create_app():
+# def create_app():
     
-    application = Flask(__name__)
-    application.config.update(
+#     application = 
+#     application.config.update(
+#     MONGO_DBNAME= os.environ.get('MONGO_DB'),
+#     MONGO_URI = os.environ.get('MONGODB'),
+#     SECRET_KEY = str(os.environ.get('SECRET_KEY')),
+#     UPLOAD_FOLDER = UP_FOLDER
+#     )
+    
+#     return application
+
+
+app = Flask(__name__)
+app.config.update(
     MONGO_DBNAME= os.environ.get('MONGO_DB'),
     MONGO_URI = os.environ.get('MONGODB'),
     SECRET_KEY = str(os.environ.get('SECRET_KEY')),
     UPLOAD_FOLDER = UP_FOLDER
     )
-    
-    return application
-
-
-app = create_app()
-
 mongo = PyMongo(app)
 
